06a70203b218dc75a6d2ef85eaaf5a883462eadc
sikehish <hisham0502@gmail.com>
2024-09-29 13:52:46 +0530

    Popup.js modified

--- google-meet-chrome-extension/popup.js
+++ google-meet-chrome-extension/popup.js
@@ -1,14 +1,14 @@
 window.onload = function () {
-  // chrome.storage.local.get(["oauthEmail"], function (result) {
-    // if (result.oauthEmail) {
-      // document.querySelector("#email").innerText = `Logged in as ${result.oauthEmail}!`;
+  chrome.storage.local.get(["oauthEmail"], function (result) {
+    if (result.oauthEmail) {
+      document.querySelector("#email").innerText = `Logged in as ${result.oauthEmail}!`;
       document.querySelector(".signed-in-only").style.display = "block";
-    // } else {
-      // document.querySelector("#email").innerText = "Not signed in!";
-      // document.querySelector(".signed-in-only").style.display = "none";
-    // }
-  // }
-  // )
+    } else {
+      document.querySelector("#email").innerText = "Not signed in!";
+      document.querySelector(".signed-in-only").style.display = "none";
+    }
+  }
+  )
 }
 
 document.getElementById('screenshot-btn').addEventListener('click', () => {
